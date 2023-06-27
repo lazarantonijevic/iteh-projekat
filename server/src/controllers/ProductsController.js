b18839c5b744918ccfa7a792c3be01f164e13b19
Marijana <makizaric52@gmail.com>
2023-06-27 20:30:05 +0200

    Cart na Front-u i Middle na Back-u

---
+++ server/src/controllers/ProductsController.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const Todos = require("../models/ProductsModal");
+
+const router = express.Router();
+
+router.post("", async (req, res) => {
+  try {
+    console.log(req.body);
+    const data = await Todos.create(req.body);
+    return res.send(data);
+  } catch (error) {
+    return res.send(500).send({ message: error.message });
+  }
+});
+
+
+
+router.get("", async (req, res) => {
+  try {
+    const data = await Todos.find().lean().exec();
+    console.log(data);
+    return res.send(data);
+  } catch (error) {
+    return res.send(error);
+  }
+});
+
+
+router.get("/id/:id", async (req, res) => {
+  console.log(req.params.id);
+  try {
+    const data = await Todos.findById(req.params.id).lean().exec();
+    console.log(data);
+    return res.send(data);
+  } catch (error) {
+    return res.send(error);
+  }
+});
+
+
+
+
+router.get("/:id", async (req, res) => {
+  try {
+    var mysort = { "price.sp": req.query.sorting || "" };
+    const rating = req.query.rating || 0;
+    const discount = req.query.discount || 0;
+
+    const user = await Todos.find().sort(mysort).lean().exec();
+
+    //const filterCtegory = user.filter((e) => e.tag === req.params.id);
+
+    const filterCtegory = user.filter((e) => {
+      if (req.params.id.includes("&&")) {
+        const [left, right] = req.params.id.split("&&");
+        return e.tag === left.trim() || e.tag === right.trim();
+      } else {
+        return e.tag === req.params.id;
+      }
+    });
+
+    
+
+    const filterRating = filterCtegory.filter(
+      (e) => e.customer_rating >= rating
+    );
+
+    const filterDiscount = filterRating.filter(
+      (e) => e.price.discount >= discount
+    );
+
+    return res.send(filterDiscount);
+  } catch (error) {
+    ``;
+    return res.send(error);
+  }
+});
+
+
+module.exports = router;
+
+
